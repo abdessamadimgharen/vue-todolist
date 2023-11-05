3bb20434e6bbd0b80a256f4ce2f2c44e8ea662c6
abdessamad <lmilanabdo@gmail.com>
2023-11-05 12:35:00 +0100

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h2>About Our To-Do List App</h2>
+    <p>
+      Welcome to our to-do list app, a simple yet powerful tool designed to help you stay organized and boost your productivity. Life can be hectic, and it's easy to feel overwhelmed by tasks and responsibilities. Our app is here to make your life a little easier.
+    </p>
+    <h3>Why Choose Us:</h3>
+    <p>
+      We are dedicated to providing you with the best to-do list experience. Our app is continually updated and improved based on user feedback, ensuring that it meets your evolving needs. We believe in helping you achieve your goals, one task at a time.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .about{
+    text-align:center;
+    h2, h3 {
+      margin: 20px 0;
+    }
+    p {
+      line-height: 2;
+      color: #777;
+      width: 80%;
+      margin: 0 auto
+    }
+  }
+
+// @media (min-width: 1024px) {
+// }
+
+</style>
